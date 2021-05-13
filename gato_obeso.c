d7468e85e493e7e2fce56048693d2b7e5c2016c6
Thierry <thierry_ara@usp.br>
2021-05-13 00:14:17 -0300

    gato_obeso_exc

--- gato_obeso.c
+++ gato_obeso.c
@@ -2,16 +2,15 @@
 
 int main () {
 
-    int mordida = 0, brincada = 0;
-    float peso_final = 0.0;
+     int mordida = 0, brincada = 0, peso_perdido = 0;
 
-    scanf ("%d %d", &brincada, &mordida);
+    scanf ("%i %i", &brincada, &mordida);
 
-    peso_final = (brincada * 5) - (mordida * 3);
-    
-    if (peso_final < 30){
+    peso_perdido = (brincada * 5) - (mordida * 3) ;
+
+    if (peso_perdido > 30){
         printf ("P");
-    } else if (peso_final == 30){ 
+    } else if (peso_perdido >= 0 && peso_perdido <= 30){ 
         printf("B");
     } else {
         printf ("R");
