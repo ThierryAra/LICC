d3d90e9f6631aad5e0e9855b00aeff020f052716
Thierry <thierry_ara@usp.br>
2021-05-19 20:39:37 -0300

    exc_07/06/21_lab_icc

--- apenas_bits.c
+++ apenas_bits.c
@@ -9,15 +9,12 @@ int main () {
 
     //como fazer pra pegar os bits iniciais????
 
-    and1 = num & 0b11111111000000000000000000000000;
-    and2 = num & 0b00000000111111110000000000000000;
-    and3 = num & 0b00000000000000001111111100000000;
-
-
+    and1 = (num >> 24) & 0b11111111;
+    and2 = (num >> 16) & 0b11111111;
+    and3 = (num >> 8) & 0b11111111;
     and4 = num & 0b11111111;
     
-    printf ("%c \n%c \n%c \n%c", and1,and2,and3,and4);   
-    printf ("\n\n%d", and4); 
+    printf ("%c%c%c%c", and1,and2,and3,and4);
 
     return 0;
 }
